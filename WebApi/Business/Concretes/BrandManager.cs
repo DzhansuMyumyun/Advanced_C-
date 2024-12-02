af3df4b9d512d05144777e94bf5d047aa8754177
Dzhansu <167971328+DzhansuMyumyun@users.noreply.github.com>
2024-12-02 22:53:59 +0100

    update

---
+++ WebApi/Business/Concretes/BrandManager.cs
@@ -0,0 +1,60 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Business.Concretes
+{
+    public class BrandManager : IBrandService
+    {
+        IBrandDal _branddal;
+        public BrandManager( IBrandDal brandDal)
+        {
+            _branddal = brandDal;
+        }
+        public CreatedBrandResponse Add(CreateBrandRequest createBrandRequest)
+        {
+            //business rules
+
+            //mapping
+            Brand brand = new Brand();
+            brand.Name = createBrandRequest.Name;
+            brand.CreatedDate = DateTime.Now;
+
+            _branddal.Add(brand);
+
+            //mapping
+            CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
+            createdBrandResponse.Name = brand.Name;
+            createdBrandResponse.Id = 3;
+            createdBrandResponse.CreatedDate = brand.CreatedDate;
+
+            return createdBrandResponse;
+        }
+
+        public List<GetAllBrandResponse> GetAll()
+        {
+           List<Brand> brands = _branddal.GetAll();
+           
+           List<GetAllBrandResponse> getAllBrandResponses = new List<GetAllBrandResponse>();
+
+            foreach (Brand brand in brands)
+            {
+                GetAllBrandResponse getAllBrandResponse = new GetAllBrandResponse();
+                getAllBrandResponse.Name = brand.Name;
+                getAllBrandResponse.Id = brand.Id;
+                getAllBrandResponse.CreatedDate = brand.CreatedDate;
+
+                getAllBrandResponses.Add(getAllBrandResponse);
+            }
+            return getAllBrandResponses;
+        }
+    }
+}
